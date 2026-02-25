fc4d6a8e1218601931b53164f78052cc0235b04d
Gustavo <gustavo@example.com>
2026-02-25 18:30:39 -0300

    Fix: Set express host to 0.0.0.0 for Render proxy

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,6 @@ import app from './app';
 
 const PORT = process.env.PORT || 3333;
 
-app.listen(PORT, () => {
+app.listen(PORT as number, '0.0.0.0', () => {
     console.log(`🚀 API rodando na porta ${PORT}`);
 });
